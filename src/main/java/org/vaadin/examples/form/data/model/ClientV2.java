e4401f4dff4b9f5a8816a4930d4be9044d1524be
regmimilan <regmi.milan@rew3.com>
2022-12-10 22:24:32 +0545

    Client Model Class

---
+++ src/main/java/org/vaadin/examples/form/data/model/ClientV2.java
@@ -0,0 +1,396 @@
+package org.vaadin.examples.form.data.model;
+
+public class ClientV2 {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private byte[] citizenShipFile;
+
+    private String email;
+    private String mobileNumber;
+    private String gender;
+    private String district;
+    private String address;
+
+    private String province;
+    private String appointmentDistrict;
+    private String location;
+    private String dobInAd;
+    private String dobInBs;
+
+    private String fatherFirstName;
+    private String fatherLastName;
+    private String motherFirstName;
+    private String motherLastName;
+
+    private String nin;
+    private String citizenshipNumber;
+    private String issueDistrict;
+    private String dateOfIssue;
+
+    //Applicant's Contact Details
+    private String applicantsProvince;
+    private String applicantsDistrict;
+    private String applicantsMunicipality;
+    private String applicantsWardNo;
+    private String applicantsVillage;
+
+    // Contact Details
+    private String contactFisrtName;
+    private String contactLastName;
+    private String contactDistrict;
+    private String contactProvince;
+    private String contactMunicipality;
+    private String contactWardNo;
+    private String contactVillage;
+
+    private byte[] citizenshipFront;
+    private byte[] citizenshipBack;
+    private byte[] marriageCertificate;
+
+
+    private boolean isNew;
+    private boolean isRenewed;
+    private boolean isLost;
+    private boolean isDamaged;
+
+    private String from;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public byte[] getCitizenShipFile() {
+        return citizenShipFile;
+    }
+
+    public void setCitizenShipFile(byte[] citizenShipFile) {
+        this.citizenShipFile = citizenShipFile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getAppointmentDistrict() {
+        return appointmentDistrict;
+    }
+
+    public void setAppointmentDistrict(String appointmentDistrict) {
+        this.appointmentDistrict = appointmentDistrict;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDobInAd() {
+        return dobInAd;
+    }
+
+    public void setDobInAd(String dobInAd) {
+        this.dobInAd = dobInAd;
+    }
+
+    public String getDobInBs() {
+        return dobInBs;
+    }
+
+    public void setDobInBs(String dobInBs) {
+        this.dobInBs = dobInBs;
+    }
+
+    public String getFatherFirstName() {
+        return fatherFirstName;
+    }
+
+    public void setFatherFirstName(String fatherFirstName) {
+        this.fatherFirstName = fatherFirstName;
+    }
+
+    public String getFatherLastName() {
+        return fatherLastName;
+    }
+
+    public void setFatherLastName(String fatherLastName) {
+        this.fatherLastName = fatherLastName;
+    }
+
+    public String getMotherFirstName() {
+        return motherFirstName;
+    }
+
+    public void setMotherFirstName(String motherFirstName) {
+        this.motherFirstName = motherFirstName;
+    }
+
+    public String getMotherLastName() {
+        return motherLastName;
+    }
+
+    public void setMotherLastName(String motherLastName) {
+        this.motherLastName = motherLastName;
+    }
+
+    public String getNin() {
+        return nin;
+    }
+
+    public void setNin(String nin) {
+        this.nin = nin;
+    }
+
+    public String getCitizenshipNumber() {
+        return citizenshipNumber;
+    }
+
+    public void setCitizenshipNumber(String citizenshipNumber) {
+        this.citizenshipNumber = citizenshipNumber;
+    }
+
+    public String getIssueDistrict() {
+        return issueDistrict;
+    }
+
+    public void setIssueDistrict(String issueDistrict) {
+        this.issueDistrict = issueDistrict;
+    }
+
+    public String getDateOfIssue() {
+        return dateOfIssue;
+    }
+
+    public void setDateOfIssue(String dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
+
+    public String getApplicantsProvince() {
+        return applicantsProvince;
+    }
+
+    public void setApplicantsProvince(String applicantsProvince) {
+        this.applicantsProvince = applicantsProvince;
+    }
+
+    public String getApplicantsDistrict() {
+        return applicantsDistrict;
+    }
+
+    public void setApplicantsDistrict(String applicantsDistrict) {
+        this.applicantsDistrict = applicantsDistrict;
+    }
+
+    public String getApplicantsMunicipality() {
+        return applicantsMunicipality;
+    }
+
+    public void setApplicantsMunicipality(String applicantsMunicipality) {
+        this.applicantsMunicipality = applicantsMunicipality;
+    }
+
+    public String getApplicantsWardNo() {
+        return applicantsWardNo;
+    }
+
+    public void setApplicantsWardNo(String applicantsWardNo) {
+        this.applicantsWardNo = applicantsWardNo;
+    }
+
+    public String getApplicantsVillage() {
+        return applicantsVillage;
+    }
+
+    public void setApplicantsVillage(String applicantsVillage) {
+        this.applicantsVillage = applicantsVillage;
+    }
+
+    public String getContactFisrtName() {
+        return contactFisrtName;
+    }
+
+    public void setContactFisrtName(String contactFisrtName) {
+        this.contactFisrtName = contactFisrtName;
+    }
+
+    public String getContactLastName() {
+        return contactLastName;
+    }
+
+    public void setContactLastName(String contactLastName) {
+        this.contactLastName = contactLastName;
+    }
+
+    public String getContactDistrict() {
+        return contactDistrict;
+    }
+
+    public void setContactDistrict(String contactDistrict) {
+        this.contactDistrict = contactDistrict;
+    }
+
+    public String getContactProvince() {
+        return contactProvince;
+    }
+
+    public void setContactProvince(String contactProvince) {
+        this.contactProvince = contactProvince;
+    }
+
+    public String getContactMunicipality() {
+        return contactMunicipality;
+    }
+
+    public void setContactMunicipality(String contactMunicipality) {
+        this.contactMunicipality = contactMunicipality;
+    }
+
+    public String getContactWardNo() {
+        return contactWardNo;
+    }
+
+    public void setContactWardNo(String contactWardNo) {
+        this.contactWardNo = contactWardNo;
+    }
+
+    public String getContactVillage() {
+        return contactVillage;
+    }
+
+    public void setContactVillage(String contactVillage) {
+        this.contactVillage = contactVillage;
+    }
+
+    public byte[] getCitizenshipFront() {
+        return citizenshipFront;
+    }
+
+    public void setCitizenshipFront(byte[] citizenshipFront) {
+        this.citizenshipFront = citizenshipFront;
+    }
+
+    public byte[] getCitizenshipBack() {
+        return citizenshipBack;
+    }
+
+    public void setCitizenshipBack(byte[] citizenshipBack) {
+        this.citizenshipBack = citizenshipBack;
+    }
+
+    public byte[] getMarriageCertificate() {
+        return marriageCertificate;
+    }
+
+    public void setMarriageCertificate(byte[] marriageCertificate) {
+        this.marriageCertificate = marriageCertificate;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
+
+    public boolean isRenewed() {
+        return isRenewed;
+    }
+
+    public void setRenewed(boolean renewed) {
+        isRenewed = renewed;
+    }
+
+    public boolean isLost() {
+        return isLost;
+    }
+
+    public void setLost(boolean lost) {
+        isLost = lost;
+    }
+
+    public boolean isDamaged() {
+        return isDamaged;
+    }
+
+    public void setDamaged(boolean damaged) {
+        isDamaged = damaged;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+}
